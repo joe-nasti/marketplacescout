35bc376200c15272a039aa301f8f737117f35f45
joe-nasti <joe.nasti@gmail.com>
2026-08-15 13:02:32 -0400

    Bump Android agent to 0.1.13

--- android-agent/app/build.gradle.kts
+++ android-agent/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.collectish.agent"
         minSdk = 28
         targetSdk = 35
-        versionCode = 13
-        versionName = "0.1.12"
+        versionCode = 14
+        versionName = "0.1.13"
     }
 
     signingConfigs {
