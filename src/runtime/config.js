9229427939ff387ccf1945a042a5a603ea2747d9
joe-nasti <joe.nasti@gmail.com>
2026-08-18 21:35:57 -0400

    refactor: move web config to ES module export

---
+++ src/runtime/config.js
@@ -0,0 +1,9 @@
+export const collectishConfig = Object.freeze({
+  supabaseUrl: 'https://bnsnlikjeogzdubgyvxk.supabase.co',
+  publishableKey: 'sb_publishable_Zl0XS3ueisENWcQAmQ0mwA_FIC4yje2'
+});
+
+// Compatibility bridge for legacy modules during the ES-module migration.
+window.COLLECTISH_CONFIG = collectishConfig;
+
+export default collectishConfig;
