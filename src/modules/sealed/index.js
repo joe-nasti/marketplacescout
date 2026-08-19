44d87c91e6c69dccff9f4b52e926fdfd1239ee00
joe-nasti <joe.nasti@gmail.com>
2026-08-19 12:47:01 -0400

    install CT0 opportunity filter

--- src/modules/sealed/index.js
+++ src/modules/sealed/index.js
@@ -4,6 +4,7 @@ export async function install(){
   installed=true;
   await import('./detail-focus.js');
   await import('./cardtrader.js');
+  await import('./cardtrader-opportunities.js');
   const module=await import('./renderer.js');
   await module.install();
 }
