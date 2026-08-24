352a4e599379fe0c9efef273b5dd62dbaafc69a2
joe-nasti <joe.nasti@gmail.com>
2026-08-23 21:27:08 -0500

    Load Scout IA v2 styles

--- src/modules/scout/index.js
+++ src/modules/scout/index.js
@@ -3,6 +3,7 @@ export async function installScoutRenderer(){
   if(installed)return;
   installed=true;
   await import('./renderer.js');
+  await import('./ia-v2-style.js');
   await import('./ia-v2.js');
 }
 export default installScoutRenderer;
