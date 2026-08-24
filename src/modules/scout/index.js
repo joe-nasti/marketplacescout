17b47b46eca791e3f5eb0c9e6b63d411b2f75eec
joe-nasti <joe.nasti@gmail.com>
2026-08-23 21:26:23 -0500

    Install Scout IA v2 controller

--- src/modules/scout/index.js
+++ src/modules/scout/index.js
@@ -1,3 +1,8 @@
 let installed=false;
-export async function installScoutRenderer(){if(installed)return;installed=true;await import('./renderer.js')}
+export async function installScoutRenderer(){
+  if(installed)return;
+  installed=true;
+  await import('./renderer.js');
+  await import('./ia-v2.js');
+}
 export default installScoutRenderer;
