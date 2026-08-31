59e963b071457a21073fb45178440ab9d4db5b38
joe-nasti <joe.nasti@gmail.com>
2026-08-31 14:12:11 -0500

    Harden Secret Lair market phase helpers

---
+++ supabase/migrations/20260831191200_harden_secret_lair_market_phase_function_search_paths.sql
@@ -0,0 +1,3 @@
+alter function public.secret_lair_market_phase(timestamptz,timestamptz,timestamptz,timestamptz) set search_path = public;
+alter function public.secret_lair_phase_equilibrium_weight(text) set search_path = public;
+alter function public.secret_lair_phase_urgency_weight(text) set search_path = public;
