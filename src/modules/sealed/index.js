1e5c8c339efa3821ee90afa70ca0c83a8a0121f6
joe-nasti <joe.nasti@gmail.com>
2026-09-02 11:00:02 -0500

    Mount sealed source compare drill-down

--- src/modules/sealed/index.js
+++ src/modules/sealed/index.js
@@ -17,11 +17,13 @@ export async function install(){
     import('./mobile-economics.js'),
     import('./renderer.js'),
     import('./out-optimizer.js'),
-    import('./product-family.js')
+    import('./product-family.js'),
+    import('./source-compare.js')
   ]);
-  const urlState=modules[5],renderer=modules[7],outOptimizer=modules[8],family=modules[9];
+  const urlState=modules[5],renderer=modules[7],outOptimizer=modules[8],family=modules[9],sourceCompare=modules[10];
   urlState.installSealedUrlState();
   outOptimizer.installOutOptimizer();
   family.install();
+  sourceCompare.installSourceCompare();
   await renderer.install();
 }
